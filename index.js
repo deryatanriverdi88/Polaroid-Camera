607182781eaaee8116fdaf423c944c9bb968efe3
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2021-01-21 00:43:40 -0500

    adds clickFunction function to index.js

--- index.js
+++ index.js
@@ -11,4 +11,22 @@ let shadow = document.querySelector("#shadow");
 function converetCanvastoImage(c){
     src = c.toDataURL('image/jpeg');
     return src
-}
+};
+
+const clickFunction = () =>{
+    img.classList.add('none');
+    img.classList.remove('metrix');
+    shadow.classList.add('none');
+    if(flashLight.classList.contains("none")){
+        context.drawImage(video, 0, 0, 640, 480);
+        flashLight.classList.remove("none");
+        img.src =  converetCanvastoImage(canvas);
+        setTimeout(() => {
+            img.classList.remove("none");
+    } , 500);
+        setTimeout(() => {
+            img.classList.add('metrix');
+            shadow.classList.remove('none');
+        }, 6000);
+    };
+};
